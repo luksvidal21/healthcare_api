b188a7ea25449e2961fca082527c999ee496e1be
Your Name <luksvidal21@gmail.com>
2023-11-18 10:30:06 -0300

    Projeto API

---
+++ src/Healthcare.Api/Controllers/PatientController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Healthcare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController : ControllerBase
+    {
+        // GET: api/<PatientController>
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET api/<PatientController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PatientController>
+        [HttpPost]
+        public IActionResult Post(ProblemDetails problemDetails)
+        {
+            HttpContext.Response.Headers.Add("X-Paginated", "{Item: 30}");
+            return CreatedAtAction("api/Patient/2",problemDetails);
+        }
+
+        // PUT api/<PatientController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PatientController>/5
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            return NotFound();
+        }
+    }
+}
